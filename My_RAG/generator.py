27e9fee61a0a1e012f113fa40a801bd513a482b6
yzkkai <yzkkai217@gmail.com>
2025-12-11 23:45:34 +0800

    fix generator

--- My_RAG/generator.py
+++ My_RAG/generator.py
@@ -7,8 +7,11 @@ def build_context(context_chunks, language):
     blocks = []
 
     for idx, (topic, sentences) in enumerate(context_chunks, start=1):
-        sentences.sort(key=lambda x: x["metadata"]["index"])
-        content = "\n".join([f"[{idx}] {sentence['page_content']}" for idx, sentence in enumerate(sentences)])
+        if isinstance(context_chunks, list):
+            sentences.sort(key=lambda x: x["metadata"]["index"])
+            content = "\n".join([f"[{idx}] {sentence['page_content']}" for idx, sentence in enumerate(sentences)])
+        else:
+            content = context_chunks
 
         if language == "zh":
             block = (
